74ea7997d5b15cb4d983a2d672a10514beb694c6
lscantelbury <lhsda.snf20@uea.edu.br>
2021-06-28 08:56:49 -0400

    Bullet Drawing

--- bullet.py
+++ bullet.py
@@ -3,16 +3,16 @@ import pygame
 
 class Bullet:
 
-    def __init__(self, parent, image, rect, speed, dx, dy):
-        self.parent = parent
-        self.image = image
-        self.rect = rect
+    def __init__(self, surface, position, speed, dx, dy):
+        self.surface = surface
+        self.position = position
         self.speed = speed
         self.dx = dx
         self.dy = dy
 
-    def update(self):
-        pass
+    def draw(self):
+        pygame.draw.circle(self.surface, (255, 0, 0), self.position, 1)
+        
 
     def collidewith(self):
         pass
